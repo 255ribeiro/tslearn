e0398c401a9826fbe0c2e9b0e1d7914b17ecb0f3
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2025-04-20 03:54:49 -0300

    rho_dcca metric

--- tslearn/metrics/det_metrics.py
+++ tslearn/metrics/det_metrics.py
@@ -15,6 +15,7 @@ def rho_dcca(x, y, tws, ignore_anti_corr):
     dfa, dcca, pdcca = zb.p_dcca(input_data.T, tws=tws, DCCA_of=dcca_of)
 
     pdcca.reshape(x.shape[0], y.shape[0])
+    print(pdcca.shape)
 
     if ignore_anti_corr == True:
         return np.power(pdcca - 1, 2)
