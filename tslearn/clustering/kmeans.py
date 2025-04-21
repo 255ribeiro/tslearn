93fbafea65b8ef64f29fc3cc736c60c621c273a9
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2025-04-20 21:25:23 -0300

    rho_dcca metric

--- tslearn/clustering/kmeans.py
+++ tslearn/clustering/kmeans.py
@@ -750,13 +750,19 @@ class TimeSeriesKMeans(
             #                 self.cluster_centers_.reshape((self.n_clusters, -1)),
             #                 tws = metric_params['tws']
             #                 )
-
-
-            return cdist(
-                X.reshape((X.shape[0], -1)),
-                self.cluster_centers_.reshape((self.n_clusters, -1)),
-                metric="euclidean",
+            # return cdist(
+            #     X.reshape((X.shape[0], -1)),
+            #     self.cluster_centers_.reshape((self.n_clusters, -1)),
+            #     metric="euclidean",
+            # )
+            return rho_dcca(
+                            x,
+                            y,
+                            tws = metric_params['tws'],
+                            ignore_anti_corr = metric_params['ignore_anti_corr'],
+                            square_values = metric_params['square_values']
             )
+
             
             
         
